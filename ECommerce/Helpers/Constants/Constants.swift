176ea440bc433bb7020e3fc3d22aad2f08b5f58d
Thaciana Lima <thacilima@Thacianas-MacBook-Pro.local>
2017-08-27 21:15:02 -0300

    Creating view controller e constants file

---
+++ ECommerce/Helpers/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ECommerce
+//
+//  Created by Thaciana Lima on 27/08/17.
+//  Copyright © 2017 TL. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct CellIds {
+        static let itemCellIdentifier = "ItemCellIdentifier"
+    }
+}
